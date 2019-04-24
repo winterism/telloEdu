b983efafe287f41553ebc9f4fc4e6e8928d42f6b
HKJeon <winterism@hotmail.com>
2019-04-24 17:32:27 +0900

    카메라스트림

--- example_3_비디오인식/main.py
+++ example_3_비디오인식/main.py
@@ -1,100 +1,155 @@
 #-*- coding: utf-8 -*-
 '''
-2019 4.22
+2019 4.24
 작성자 한국교원대학교 컴퓨터교육과 전형기
-가속도 적용 인터페이스
+단순동작
 ----------키 설명----------- 
 화살표 위 아래 : 고도 변경
 화살표 좌 우 : 좌우 각도 변경
 w s a d : 앞/뒤/왼쪽/오른쪽
-'''
 
+참고 
+'''
 from tello import Tello #tello 모듈로 부터 Tello를 불러온다.
-from tello import VideoStreamer
-import sys
+import cv2 
 import time # 시간 관련 모듈
 import tkinter
+import pygame
+from pygame.locals import *
+import numpy as np
+bg_color = (128, 128, 128)
+black = (0, 0, 0)
+blue = (0, 50, 255)
+white = (255, 255, 255)
+red = (255, 0, 0)
+green = (0, 200, 0)
 
-tello = Tello() # tello 인스턴스 를 생성한다. tello모듈에 객체의 명세가 기록되어 있다.
-video = VideoStreamer()  # 스트림 인스턴스 생성한다. 
-tello.sendCmd("command")
+FPS = 25
+S = 60
 
-def keyCmd(cmd):
-  if cmd=="takeoff" or cmd=="land" or cmd=="command":
-    tello.sendCmd(cmd)
-    tello.state = "takeoff"
-  elif cmd=="land":
-    tello.sendCmd(cmd)
-    tello.state = "land"
-  elif cmd=="command":
-    tello.sendCmd(cmd)
-    tello.state = "ready"
-  else:
-    tello.sendCmd(cmd+" 20")
-
-def speedNormalize(value,cmd,multiply):
-  """
-  이 함수는 키 눌림에 따라 하여 반환
-  반영되는 속도는 최고 속도를 초과하지 않음
-  """  
-  if abs(value+cmd * multiply) < tello.max_speed:
-    value = value + cmd * multiply
-  else :
-    value = tello.max_speed * (abs(value)/value)
-  return value
-
-def accelHeight(cmd):
-  """
-  이 함수는 키보드 눌림에 따라 텔로의 상하 이동 속도에 반영
-  """  
-  tello.speed_ud = speedNormalize(tello.speed_ud,cmd,15)
-
-def accelYaw(cmd):
-  """
-  이 함수는 키보드 눌림에 따라 텔로의 좌우 회전 속도에 반영
-  """
-  tello.speed_yaw = speedNormalize(tello.speed_yaw,cmd,15)
-
-def accelFB(cmd):
-  """
-  이 함수는 키보드 눌림에 따라 텔로의 앞뒤 이동 속도에 반영
-  """
-  tello.speed_fb = speedNormalize(tello.speed_fb,cmd,15)
-
-def accelLR(cmd):
-  """
-  이 함수는 키보드 눌림에 따라 텔로의 왼쪽 오른쪽 이동 속도에 반영
-  """
-  tello.speed_lr = speedNormalize(tello.speed_lr,cmd,15)
-
-def brake():
-  tello.contrlNoReturn("stop")
-
-def streamVideoStart():
-  tello.contrlNoReturn("streamon")
-  video.startVideoStream()
-
-"""
-이 아래는 키보드 맵핑과 gui 화면에 관련된 내용
-"""
-window=tkinter.Tk()
-label=tkinter.Label(window, text=tello.state)
-label.pack()
-canvas=tkinter.Canvas(window, relief="solid", bd=2)
-canvas.pack(expand=True, fill="both")
-
-window.bind('<Return>', lambda _: keyCmd("command")) #
-window.bind('<Shift-Up>', lambda _: keyCmd("takeoff")) #
-window.bind('<Shift-Down>', lambda _: keyCmd("land")) #
-window.bind('<Up>', lambda _: accelHeight(1)) #
-window.bind('<Down>', lambda _: accelHeight(-1)) #
-window.bind('<Right>', lambda _: accelYaw(1)) #
-window.bind('<Left>', lambda _: accelYaw(-1)) #
-window.bind('a', lambda _: accelLR(-1)) #
-window.bind('d', lambda _: accelLR(1)) #
-window.bind('w', lambda _: accelFB(1)) #
-window.bind('s', lambda _: accelFB(-1)) #
-window.bind('[', lambda _: streamVideoStart()) #
+class Interface: 
+   
+    def __init__(self):
+        self.tello = Tello()
+        self.tello.sendCmd("streamon") #스트리밍 모드
+        pygame.init()
+        pygame.display.set_caption("KNUE Drone Stream")
+        self.screen = pygame.display.set_mode([960, 720]) 
+        pygame.time.set_timer(USEREVENT + 1, 50)
+        
+        self.font = pygame.font.Font("freesansbold.ttf", 14)
+        
+    def make_text(self, font, text, color, bgcolor, top, left, position = 0):
+        surf = font.render(text, False, color, bgcolor)
+        rect = surf.get_rect()
+        if position:
+            rect.center = (left, top)
+        else:    
+            rect.topleft = (left, top)
+        self.screen.blit(surf, rect)
+        return rect
 
 
-window.mainloop()
+    def run(self):
+        '''
+        프레임을 업데이트 하고 이벤트를 처리한다.
+        '''
+        frame_read = self.tello.get_frame_read()
+        time.sleep(1)
+        exitEvent = False
+        previousCounter = 0
+        while not exitEvent:
+            #이벤트 검사
+            for event in pygame.event.get():
+                if event.type == USEREVENT + 1:
+                    pass
+                elif event.type == KEYDOWN:
+                    keys = pygame.key.get_pressed()
+                    self.keyDown(keys)
+                elif event.type == KEYUP:
+                    self.keyUp(keys)            
+            if frame_read.stopped:
+                frame_read.stop()
+                break
+            nowCounter = frame_read.frameCounter
+            if nowCounter > previousCounter :
+                self.screen.fill([0, 0, 0])                     # 스크린 칠하기(검정)
+                frame = cv2.cvtColor(frame_read.frame, cv2.COLOR_BGR2RGB)   # 읽어온프레임의 색 형식 바꾸기
+                frame = np.rot90(frame)                         # 90도 회전    
+                frame = np.flipud(frame)                        # 위아래 전환
+                face = pygame.surfarray.make_surface(frame)    # 화면 형식에 맞게 전환하고
+                self.screen.blit(face, (0, 0))                 # 탐색필요
+                self.frame_rect = self.make_text(self.font, "frameCount : "+str(nowCounter), blue, None, 20, 10)
+                self.bat_rect = self.make_text(self.font, "batteryState : "+str(self.tello.battery), green, None, 40, 10)
+                self.wifi_rect = self.make_text(self.font, "wifiState : "+str(self.tello.wifi), green, None, 60, 10)
+                pygame.display.update()
+                previousCounter =  frame_read.frameCounter     # 화면갱신
+            pygame.display.update()
+            time.sleep(1 / FPS)
+
+    def keyDown(self, key):
+        if key[pygame.K_HOME]: 
+            self.tello.state = "takeoff"
+        if key[pygame.K_END]: 
+            self.tello.sendCmd("land")
+            self.tello.state = "land"
+        if key[pygame.K_UP]:  
+            self.tello.speed_ud = 50
+            print("key up")
+        if key[pygame.K_DOWN]:
+            self.tello.speed_ud = -50
+            print("key down")
+        if key[pygame.K_LEFT]:
+            self.tello.speed_yaw = -50
+            print("key left")
+        if key[pygame.K_RIGHT]:
+            self.tello.speed_yaw = 50
+            print("key right")
+        if key[pygame.K_w]:
+            self.tello.speed_fb = 50
+            print("key foward")
+        if key[pygame.K_s]:
+            self.tello.speed_fb = -50
+            print("key back")
+        if key[pygame.K_a]:
+            self.tello.speed_lr = -50
+        if key[pygame.K_d]:
+            self.tello.speed_lr = 50
+
+    def keyUp(self, key):
+        if key[pygame.K_UP]:  
+            self.tello.speed_ud = 0
+        if key[pygame.K_DOWN]:
+            self.tello.speed_ud = 0
+        if key[pygame.K_LEFT]:
+            self.tello.speed_yaw = 0
+        if key[pygame.K_RIGHT]:
+            self.tello.speed_yaw = 0
+        if key[pygame.K_w]:
+            self.tello.speed_fb = 0
+        if key[pygame.K_s]:
+            self.tello.speed_fb = 0
+        if key[pygame.K_a]:
+            self.tello.speed_lr = 0
+        if key[pygame.K_d]:
+            self.tello.speed_lr = 0
+
+
+    def speedNormalize(self,value,cmd,multiply):
+        """
+        이 함수는 키 눌림에 따라 하여 반환
+        반영되는 속도는 최고 속도를 초과하지 않음
+        """  
+        if abs(value+cmd * multiply) < self.tello.max_speed:
+            value = value + cmd * multiply
+        else :
+            value = self.tello.max_speed * (abs(value)/value)
+        return value
+
+def main():
+    interface = Interface()
+    # run frontend 
+    interface.run()
+
+if __name__ == '__main__':
+    main()
--- example_3_비디오인식/tello.py
+++ example_3_비디오인식/tello.py
@@ -1,114 +1,51 @@
 '''
+2019 4.24
+
 Tello class 설명
   __init__(self) : 생성자 
   send_command(self, command) :  명령 보내기
   _receive_thread(self) : 텔로로 부터 받은 가장 최근의 것을 self.response에 저장한다. 
   get_log() : 로그 받기
+
+참고 : https://zzsza.github.io/data/2018/01/23/opencv-1/
 '''
 
 import socket
 import threading
 import time
-from stats import Stats
-import cv2.cv2 as cv2
-import numpy as np
-# import libh264decoder
+import cv2
 
-class VideoStreamer:
-    stream_ip = '0.0.0.0'
-    stream_port = 11111
-    width = 1280
-    height = 720
-    buff = 2048
-    num_of_chunks = width * height * 3 /buff
-    code = b'start'
-
-    def __init__(self):
-        # 비디오스트림을 받기 위한 서버 소켓 설정
-        self.streamSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # 스트림 소켓 생성
-        self.streamSocket.bind((self.stream_ip, self.stream_port)) #소켓을 IP와 포트에 바인드
-        # self.decoder = libh264decoder.H264Decoder()
-        self.frame = None
-
-    def videoStream(self):
-        while True:
-            chunks = []
-            start = False
-            packet_data = ""
-            while True:
-                res_string, ip = self.streamSocket.recvfrom(2048)
-                packet_data += res_string
-
-                # chunk, _ = self.streamSocket.recvfrom(2048)
-
-                # if start:
-                #     chunks.append(chunk)
-                # elif chunk.startswith(self.code):
-                #     start = True
-
-                byte_frame = b''.join(packet_data)
-
-                frame = np.frombuffer(
-                    byte_frame, dtype=np.uint8).reshape(self.height, self.width, 3)
-                cv2.imshow('recv', frame)
-                if cv2.waitKey(1) & 0xFF == ord('q'):
-                    break
-
- 
-    #             # end of frame
-    #             if len(res_string) != 1460:
-    #                 for frame in self._h264_decode(packet_data):
-    #                     self.frame = frame
-    #                 packet_data = ""
-
-    # def _h264_decode(self, packet_data):
-    #     """
-    #     decode raw h264 format data from Tello
-        
-    #     :param packet_data: raw h264 data array
-       
-    #     :return: a list of decoded frame
-    #     """
-    #     res_frame_list = []
-    #     frames = self.decoder.decode(packet_data)
-    #     for framedata in frames:
-    #         (frame, w, h, ls) = framedata
-    #         if frame is not None:
-    #             # print 'frame size %i bytes, w %i, h %i, linesize %i' % (len(frame), w, h, ls)
-
-    #             frame = np.fromstring(frame, dtype=np.ubyte, count=len(frame), sep='')
-    #             frame = (frame.reshape((h, ls / 3, 3)))
-    #             frame = frame[:, :w, :]
-    #             res_frame_list.append(frame)
-    #     return res_frame_list
-
-    def startVideoStream(self):
-        self.video_thread = threading.Thread(target=self.videoStream) #_receive_thread를 별도 쓰레드로 지정
-        self.video_thread.daemon = True # 데몬으로 작동하도록 설정, 데몬쓰레드는 메인쓰레드가 끝나면 끝난다.
-        self.video_thread.start()
+# from stats import Stats
 
 class Tello:
-    state = ''
-   
+
+    state = ""
+    cap = None
+    background_frame_read = None
+    stream_on = False
+    VS_UDP_IP = '0.0.0.0'
+    VS_UDP_PORT = 11111
+    streamAddress = 'udp://@0.0.0.0:11111'
+    wifi = 0
+    battery = 0
+    tello_ip = '192.168.10.1'
+    tello_port = 8889
+    tello_adderss = (tello_ip, tello_port)
+    freamCounter = 0
+
     def __init__(self):
-        self.local_ip = ''
+        self.local_ip = ""
         self.local_port = 8889
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # 명령 소켓 생성
         self.socket.bind((self.local_ip, self.local_port)) #소켓을 로컬IP와 포트에 바인드
-        # 응답이 보고될 쓰레드 생성하고 쓰레드 시작
-        self.receive_thread = threading.Thread(target=self._receive_thread) #_receive_thread를 별도 쓰레드로 지정
-        self.receive_thread.daemon = True # 데몬으로 작동하도록 설정, 데몬쓰레드는 메인쓰레드가 끝나면 끝난다.
-        self.receive_thread.start()
-
+        self.sendCmd("command") #sdk모드로 전환
+        self.state = "ready"
         self.control_thread = threading.Thread(target=self._control_thread) #_receive_thread를 별도 쓰레드로 지정
         self.control_thread.daemon = True # 데몬으로 작동하도록 설정, 데몬쓰레드는 메인쓰레드가 끝나면 끝난다.
         self.control_thread.start()
-        self.state = 'ready'
 
         #텔로 아이피 지정
-        self.tello_ip = '192.168.10.1'
-        self.tello_port = 8889
-        self.tello_adderss = (self.tello_ip, self.tello_port)
+
         self.log = []
         #타임아웃 시간 15초
         self.MAX_TIME_OUT = 15.0
@@ -123,25 +60,34 @@ class Tello:
         텔로 ip에 명령을 보냅니다. OK가 수신될때까지 이 함수에 머물게 됩니다.
         즉, 보낸명령이 수행되었다는 보고를 받기 전까진 다른 명령을 보낼 수 없습니다.
         """
-        self.log.append(Stats(command, len(self.log))) #로그 저장
         self.socket.sendto(command.encode('utf-8'), self.tello_adderss) #명령을 보냄. 최중요 코드
-        print('명령을 보내는중... 명령: %s IP %s'%(command, self.tello_ip))
-        print("명령을 입력할 수 없음")
+        self.response, ip = self.socket.recvfrom(1024)
+        print("response : "+str(self.response))
 
-        #현재시간과 지연시간을 비교하여 초과하면 블록됨
-        start = time.time() 
-        while not self.log[-1].got_response():
-            now = time.time()
-            diff = now - start
-            if diff > self.MAX_TIME_OUT:
-                print('최대 대기시간을 초과했습니다... command %s'%(command))
-                return
         
-        #현재시간과 지연시간을 비교하여 초과하면 블록됨
-        print('명령 보내기 성공: %s to %s'%(command, self.tello_ip))
+    def get_udp_video_address(self):
+        return 'udp://@' + self.VS_UDP_IP + ':' + str(self.VS_UDP_PORT)  # + '?overrun_nonfatal=1&fifo_size=5000'
 
-    def contrlNoReturn(self, command):
-        self.socket.sendto(command.encode('utf-8'), self.tello_adderss)
+    def get_video_capture(self):
+        """Get the VideoCapture object from the camera drone
+        Returns:
+            VideoCapture
+        """
+        if self.cap is None:
+            self.cap = cv2.VideoCapture(self.get_udp_video_address())
+
+        if not self.cap.isOpened():
+            self.cap.open(self.get_udp_video_address())
+
+        return self.cap
+
+    def get_frame_read(self):
+        """
+        백그라운드 프레임 가져오기
+        """
+        if self.background_frame_read is None:
+            self.background_frame_read = BackgroundFrameRead(self, self.get_udp_video_address()).start()
+        return self.background_frame_read
 
     def _control_thread(self):
         """
@@ -149,52 +95,63 @@ class Tello:
         반영되는 속도는 최고 속도를 넘지 않습니다.
         takeoff 상태에서만 작동합니다.
         """
+        controlCounter = 0
+
         while True:
+            print("텔로 상태"+self.state)
             if self.state == "takeoff":
+                # self.sendCmd("takeoff")
+                self.socket.sendto("takeoff".encode('utf-8'), self.tello_adderss)
+                self.response, ip = self.socket.recvfrom(1024)
+                if self.response == b'ok':
+                    self.state = "flying"
+            if self.state == "flying":
                 msg = ("rc %d %d %d %d" %(self.speed_lr, self.speed_fb, self.speed_ud, self.speed_yaw))
                 self.socket.sendto(msg.encode('utf-8'), self.tello_adderss)
-                time.sleep(0.1)
-                print ("rc %d %d %d %d" %(self.speed_lr, self.speed_fb, self.speed_ud, self.speed_yaw))
-
-                if abs(self.speed_fb) > 20:
-                    temp = self.speed_fb
-                    self.speed_fb=(abs(temp)/2)*(abs(temp)/temp)
-                else :
-                    self.speed_fb = 0
-                if abs(self.speed_lr) > 20:
-                    temp = self.speed_lr
-                    self.speed_lr=(abs(temp)/2)*(abs(temp)/temp)
-                else :
-                    self.speed_lr = 0
-                if abs(self.speed_ud) > 20:
-                    temp = self.speed_ud
-                    self.speed_ud=(abs(temp)/2)*(abs(temp)/temp)
-                else :
-                    self.speed_ud = 0
-                if abs(self.speed_yaw) > 20:
-                    temp = self.speed_yaw
-                    self.speed_yaw=(abs(temp)/2)*(abs(temp)/temp)
-                else :
-                    self.speed_yaw = 0
-
-            else:
-                print("전송안함")
-                time.sleep(1)
-
-    def _receive_thread(self):
-        """
-        별도 쓰레드로 작동하며 명령에 대해 텔로로 부터 들은 응답이 보고됩니다.
-        """
-        while True:
-            try:
-                self.response, ip = self.socket.recvfrom(1024)
-                print('IP %s 로 부터 온 응답: %s'%(ip, self.response))
-                self.log[-1].add_response(self.response)
-            except socket.error as exc:
-                print("소켓 에러 발생 : %s"%(exc))
+                print ("제어 %d %d %d %d" %(self.speed_lr, self.speed_fb, self.speed_ud, self.speed_yaw))
+            self.socket.sendto("wifi?".encode('utf-8'), self.tello_adderss)
+            self.wifi, ip = self.socket.recvfrom(1024)
+            self.socket.sendto("battery?".encode('utf-8'), self.tello_adderss)
+            self.battery, ip = self.socket.recvfrom(1024)
+            time.sleep(0.2)
 
     def on_close(self):
         pass
 
     def get_log(self):
         return self.log
+
+class BackgroundFrameRead:
+    """
+    This class read frames from a VideoCapture in background. Then, just call backgroundFrameRead.frame to get the
+    actual one.
+    """
+    frameCounter = 0
+
+    def __init__(self, tello, address):
+        tello.cap = cv2.VideoCapture(address)
+        self.cap = tello.cap
+
+        if not self.cap.isOpened():
+            self.cap.open(address)
+
+        self.grabbed, self.frame = self.cap.read()
+        self.stopped = False
+        self.frameCounter = 0
+
+    def start(self):
+        streamingThread = threading.Thread(target=self.update_frame, args=())
+        streamingThread.daemon = True
+        streamingThread.start()
+        return self
+
+    def update_frame(self):
+        while not self.stopped:
+            if not self.grabbed or not self.cap.isOpened(): # 읽히지 않았는지 또는 열리지 않았는지
+                self.stop()
+            else:
+                (self.grabbed, self.frame) = self.cap.read() # grabbed가 True면 읽힌것, frame에는 읽은 프레임이 들어간다.
+                self.frameCounter = self.frameCounter + 1
+        
+    def stop(self):
+        self.stopped = True
